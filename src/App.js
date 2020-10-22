feb4997fcc97f4b65a33b4cbeb2f05e0641ff691
kandaguru <kandaguru@tradewindow.io>
2020-10-23 01:59:18 +1300

    Added default route to handle 404

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import ViewPostDetails from './container/ViewPostDetail';
 import ViewPosts from './container/ViewPosts';
+
+function NotFound() {
+  return (
+    <>
+      <h1>Page not found</h1>
+      <Link to='/all'>
+        <h4>click here</h4>
+      </Link>
+    </>
+  );
+}
 
 function App() {
   return (
@@ -11,6 +22,7 @@ function App() {
         <Switch>
           <Route path='/all' exact component={ViewPosts} />
           <Route path='/posts/:id' exact component={ViewPostDetails} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     </div>
